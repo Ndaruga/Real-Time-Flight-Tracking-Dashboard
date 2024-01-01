f77c95412c5f638aaa21da766cd7e5f58e040b16
ndarugaa <ndaruga.francis@gmail.com>
2024-01-01 20:16:58 +0300

    get api data in json

--- Azure Function app/function_app.py
+++ Azure Function app/function_app.py
@@ -9,6 +9,8 @@ def timer_trigger_10_mins(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
     
-    api
+    API_KEY = "ec3717ce-36bf-46b8-b8fe-356638bd8236"
+    BASE_URL = f"https://airlabs.co/api/v9/flight?api_key={API_KEY}"
+    
 
     logging.info('Python timer trigger function executed.')
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 pip install azure-core
 pip install azure-keyvault
 pip install azure-identity

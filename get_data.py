7e9ddd85bf5e20f7183e02d9205a4338e754cca7
ndaruga <ndaruga.francis@gmail.com>
2024-04-30 05:20:03 +0300

    Create postgres Database

---
+++ get_data.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
+def create_spark_session():
+    return(
+        SparkSession
+        .builder
+        .appName("Flights")
+        .master("local[5]")
+        .getOrCreate()
+    )
+
+spark = create_spark_session()
+url=f"https://airlabs.co/api/v9/flights?api_key={os.environ.get("API_KEY")}"
+flightrawdata = requests.request('GET', url)
+print(flightrawdata.json())
+
+
+# Stop spark session
+spark.stop()
+print("spark session stopped")

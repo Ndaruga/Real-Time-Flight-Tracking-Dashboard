af7ffd04fa343a6e09758ff5b165362b05861f76
ndaruga <ndaruga.francis@gmail.com>
2024-04-30 23:31:18 +0300

    write to postgres database

--- CSVtoSQL.py
+++ CSVtoSQL.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 import psycopg2
+from sqlalchemy import create_engine
 from psycopg2 import OperationalError
 import time
 
@@ -9,22 +10,6 @@ load_dotenv()
 
 DATABASE_NAME = "World_airports"
 
-def connect_to_database():
-    try:
-        conn = psycopg2.connect(
-            database=DATABASE_NAME,
-            user="frank",
-            password=os.environ.get("POSTGRES_PASSWORD"),
-            host=os.environ.get("POSTGRES_HOST"),
-            port=os.environ.get("POSTGRES_PORT")
-        )
-        conn.autocommit = True
-        return conn
-    except OperationalError as e:
-        if "database \"%s\" does not exist" % DATABASE_NAME in str(e):
-            return None
-        else:
-            raise e
 
 # Connect to default database to create user role and drop/recreate the target database
 conn_default = psycopg2.connect(
@@ -38,45 +23,38 @@ conn_default.autocommit = True
 cursor_default = conn_default.cursor()
 
 # Drop existing database, if any, and create a new one
-cursor_default.execute(f"DROP DATABASE IF EXISTS {DATABASE_NAME}")
+cursor_default.execute(f"DROP DATABASE IF EXISTS {DATABASE_NAME.lower()}")
 cursor_default.execute("CREATE ROLE frank WITH LOGIN PASSWORD %s", (os.environ.get("POSTGRES_PASSWORD"),))
-cursor_default.execute(f"CREATE DATABASE {DATABASE_NAME} OWNER frank")
-cursor_default.execute(f"GRANT ALL PRIVILEGES ON DATABASE {DATABASE_NAME} TO frank")
-print("Database created.")
+cursor_default.execute(f"CREATE DATABASE {DATABASE_NAME.lower()} OWNER frank")
+cursor_default.execute(f"GRANT ALL PRIVILEGES ON DATABASE {DATABASE_NAME.lower()} TO frank")
+print(f"{DATABASE_NAME} Database created.\n")
 
 # Close connection to default database
 conn_default.close()
 
-# Retry connection until it succeeds or the maximum number of retries is reached
-max_retries = 10
-retry_delay = 2  # Delay in seconds between retries
-retries = 0
 
-while retries < max_retries:
-    conn_db = connect_to_database()
-    if conn_db:
-        break
+# CONNECT TO THE NEWLY CREATED DATABASE
+try:
+    conn_string = f'postgresql://{os.environ.get("POSTGRES_USER")}:{os.environ.get("POSTGRES_PASSWORD")}@{os.environ.get("POSTGRES_HOST")}/{DATABASE_NAME.lower()}'
+    print(f"Connected to {DATABASE_NAME.lower()} database...\n")
+except OperationalError as e:
+    if "database \"%s\" does not exist" % DATABASE_NAME in str(e):
+        print(f"Database '{DATABASE_NAME}' does not exist on this server.")
     else:
-        print(f"Database '{DATABASE_NAME}' not yet available. Retrying in {retry_delay} seconds...")
-        retries += 1
-        time.sleep(retry_delay)
+        print(f"Error connecting to database '{DATABASE_NAME}': {e}")
 
-if retries == max_retries:
-    print(f"Failed to connect to database '{DATABASE_NAME}' after {max_retries} retries. Exiting...")
-    exit()
+db = create_engine(conn_string) 
+conn = db.connect()
 
-# Connect to the newly created database
-conn_db = connect_to_database()
-cursor_db = conn_db.cursor()
 
 # Read CSV files and upload to PostgreSQL
 for filename in os.listdir("./data"):
     if filename.endswith(".csv"):
-        table_name = os.path.splitext(filename)[0]  # Extract table name from filename
-        with open(f"data/{filename}", 'r') as f:
-            next(f)  # Skip the header
-            cursor_db.copy_from(f, table_name, sep=',')
-            print(f"Data loaded into {table_name}.")
+        data= pd.read_csv(f"data/{filename}")
+        table_name = filename.split(".")[0]
+        # converting data to sql 
+        data.to_sql(table_name, conn, if_exists= 'replace')
+        print(f"Data loaded into '{table_name}' table.")
 
 # Close connection to the database
-conn_db.close()
+conn.close()

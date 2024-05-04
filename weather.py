39156e00fd640154236b5d0fbd178f246f438b43
ndaruga <ndaruga.francis@gmail.com>
2024-05-04 19:15:59 +0300

    send data to kafka topic

--- weather.py
+++ weather.py
@@ -8,21 +8,30 @@ Read more about pricing here https://openweathermap.org/price#weather
 '''
 
 import os
+import json
 import time
 import requests
+from kafka import KafkaProducer
 from dotenv import load_dotenv
 
 load_dotenv(override=False)
 
 weather_api_key = os.environ.get('WEATHER_API_KEY')
-
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
 
+producer= KafkaProducer(
+    bootstrap_servers=f"{os.environ.get("HOST")}:29092"
+)
+
 def get_city_weather_data(city_name):
+    print(f"Sending weather data from {city_name}")
     URL = f"{BASE_URL}{city_name}&appid={weather_api_key}"
     req = requests.get(URL)
-
-    print(req.json())
+    producer.send(
+        os.environ.get("WEATHER_TOPIC"),
+        json.dumps(req, indent=4, sort_keys=True, default=str).encode('utf-8')
+    )
+# print(data)
 
 get_city_weather_data(city_name='Nairobi')
 
